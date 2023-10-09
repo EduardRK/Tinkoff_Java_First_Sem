ef09d6993cb54038d08babab573ced5f6ba335e4
EduardRK <Eduard.ivanushkin.100@gmail.com>
2023-10-09 15:43:28 +0300

    Added Tasks 0-8 and Tests 1-8

--- src/main/java/edu/hw1/Main.java
+++ src/main/java/edu/hw1/Main.java
@@ -1,39 +1,9 @@
 package edu.hw1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public final class Main {
     private Main() {
     }
 
-    private final static Logger LOGGER = LogManager.getLogger();
-
     public static void main(String[] args) {
-
-//        Task7
-//        LOGGER.info(Task7.rotateRight(8, 1));
-//        LOGGER.info(Task7.rotateRight(10, -9));
-//        LOGGER.info(Task7.rotateRight(-100, 1));
-//        LOGGER.info(Task7.rotateRight(900, 4));
-//        LOGGER.info(Task7.rotateLeft(16, 1));
-//        LOGGER.info(Task7.rotateLeft(17, 2));
-//        LOGGER.info(Task7.rotateLeft(17, -4));
-//        LOGGER.info(Task7.rotateLeft(-2, 2));
-//        LOGGER.info(Task7.rotateLeft(34, 1));
-//        LOGGER.info(Task7.rotateLeft(25, 7));
-
-        //        Task_1
-//        LOGGER.info(Task_1.minutesToSeconds("123:12")); // true
-//        LOGGER.info(Task_1.minutesToSeconds("3:59")); // true
-//        LOGGER.info(Task_1.minutesToSeconds("-123:12")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("  123:12 ")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("  12:3:12 ")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("dogfood")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("1310")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("123:100")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("10:1s2")); // false
-//        LOGGER.info(Task_1.minutesToSeconds("0:0")); // true
     }
 }
