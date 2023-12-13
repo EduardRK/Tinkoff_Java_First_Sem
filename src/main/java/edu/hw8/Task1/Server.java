deb70fe0e4dc218f2509bd8eb010edb66377e3a5
EduardRK <Eduard.ivanushkin.100@gmail.com>
2023-12-13 19:27:16 +0300

    Add Task1 and Task1Test.

---
+++ src/main/java/edu/hw8/Task1/Server.java
@@ -0,0 +1,58 @@
+package edu.hw8.Task1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static final String END_MESSAGE = "end";
+    private final ExecutorService executorService =
+        Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private final int port;
+    private boolean isWork = true;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void work() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (isWork) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    executorService.submit(() -> {
+                        try (
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))
+                        ) {
+
+                            String request = bufferedReader.readLine();
+
+                            bufferedWriter.write(KeyWords.KEY_WORDS.getOrDefault(request, "На это ответить нечего"));
+                            bufferedWriter.newLine();
+                            bufferedWriter.flush();
+
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                } catch (IOException ignored) {
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+
+    public void stopWork() {
+        isWork = false;
+    }
+}
+
---
+++ src/test/java/edu/hw8/Task1Test.java
@@ -0,0 +1,80 @@
+package edu.hw8;
+
+import edu.hw8.Task1.Client;
+import edu.hw8.Task1.Server;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class Task1Test {
+    private static final int PORT = 8080;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime()
+        .availableProcessors());
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "личности, Не переходи на личности там, где их нет",
+        "оскорбления, Если твои противники перешли на личные оскорбления, будь уверена — твоя победа не за горами",
+        "глупый, А я тебе говорил, что ты глупый? Так вот, я забираю свои слова обратно... Ты просто бог идиотизма.",
+        "интеллект, Чем ниже интеллект, тем громче оскорбления"
+    })
+    @DisplayName("Test single request")
+    public void singleRequest(String request, String expectedResponse) {
+        Server server = new Server(PORT);
+        Client client = new Client(PORT);
+
+        Thread thread = new Thread(server::work);
+        thread.start();
+
+        executorService.submit(() -> Assertions.assertEquals(client.getResponse(request), expectedResponse));
+        executorService.shutdown();
+
+        server.stopWork();
+    }
+
+    @Test
+    @DisplayName("Test multi request")
+    public void multiRequest() {
+        List<String> words = new ArrayList<>(List.of("личности", "оскорбления", "глупый", "интеллект", "wrong"));
+        List<String> responses = new CopyOnWriteArrayList<>();
+        Set<String> responsesSet = new HashSet<>(Set.of(
+            "Не переходи на личности там, где их нет",
+            "Если твои противники перешли на личные оскорбления, будь уверена — твоя победа не за горами",
+            "А я тебе говорил, что ты глупый? Так вот, я забираю свои слова обратно... Ты просто бог идиотизма.",
+            "Чем ниже интеллект, тем громче оскорбления",
+            "На это ответить нечего"
+        ));
+
+        Server server = new Server(PORT);
+        Thread thread = new Thread(server::work);
+        thread.start();
+
+        for (int i = 0; i < 100; ++i) {
+            executorService.submit(() -> {
+                ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+                Client client = new Client(PORT);
+                String response = client.getResponse(words.get(threadLocalRandom.nextInt(words.size())));
+                responses.add(response);
+            });
+        }
+
+        executorService.close();
+
+        for (String string : responses) {
+            Assertions.assertTrue(responsesSet.contains(string));
+        }
+
+        server.stopWork();
+        Assertions.assertEquals(100, responses.size());
+    }
+}
