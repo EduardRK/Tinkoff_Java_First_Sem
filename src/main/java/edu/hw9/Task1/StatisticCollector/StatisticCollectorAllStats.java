1a2047aa87563df7de3543ae2375668942cdca9b
EduardRK <Eduard.ivanushkin.100@gmail.com>
2023-12-13 22:18:42 +0300

    Add multi thread stats counting

--- src/main/java/edu/hw9/Task1/StatisticCollector/StatisticCollectorAllStats.java
+++ src/main/java/edu/hw9/Task1/StatisticCollector/StatisticCollectorAllStats.java
@@ -2,20 +2,24 @@ package edu.hw9.Task1.StatisticCollector;
 
 import edu.hw9.Task1.StatisticFactory;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class StatisticCollectorAllStats implements StatisticCollector<Map<String, Map<String, Double>>> {
     private static final List<String> METRICS = new ArrayList<>(List.of("max", "min", "sum", "average"));
+    private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
     private final Map<String, Map<String, Double>> statistic = new ConcurrentHashMap<>();
 
     @Override
     public void push(String metricName, double[] values) {
-        Map<String, Double> map = new HashMap<>();
+        Map<String, Double> map = new ConcurrentHashMap<>();
         for (String metric : METRICS) {
-            map.put(metric, StatisticFactory.createStatistic(metric, values).getStat());
+            executorService.submit(() -> {
+                map.put(metric, StatisticFactory.createStatistic(metric, values).getStat());
+            });
         }
         statistic.put(metricName, map);
     }
