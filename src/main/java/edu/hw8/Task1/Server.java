4dfd28497b171973ff1254f5ccd779eb90059691
EduardRK <Eduard.ivanushkin.100@gmail.com>
2023-12-13 19:40:43 +0300

    Fix checkstyle

--- src/main/java/edu/hw8/Task1/Server.java
+++ src/main/java/edu/hw8/Task1/Server.java
@@ -28,8 +28,12 @@ public class Server {
                     Socket socket = serverSocket.accept();
                     executorService.submit(() -> {
                         try (
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))
+                            BufferedReader bufferedReader = new BufferedReader(
+                                new InputStreamReader(socket.getInputStream())
+                            );
+                            BufferedWriter bufferedWriter = new BufferedWriter(
+                                new OutputStreamWriter(socket.getOutputStream())
+                            )
                         ) {
 
                             String request = bufferedReader.readLine();
