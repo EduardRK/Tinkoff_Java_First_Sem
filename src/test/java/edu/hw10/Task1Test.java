3239b039fa3e9be29e993e99c8e8e4d3e9e6fd20
EduardRK <Eduard.ivanushkin.100@gmail.com>
2023-12-15 14:07:25 +0300

    Add tests

--- src/test/java/edu/hw10/Task1Test.java
+++ src/test/java/edu/hw10/Task1Test.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 
 public class Task1Test {
     @Test
-    @DisplayName("Test random class generator")
-    public void randomClassGenerator() {
+    @DisplayName("Test random class generator constructor")
+    public void randomClassGeneratorConstructor() {
         RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator(MyClass.class);
 
         for (int i = 0; i < 1_000; ++i) {
@@ -21,7 +21,19 @@ public class Task1Test {
     }
 
     @Test
-    @DisplayName("Test random record generator")
+    @DisplayName("Test random class generator factory method")
+    public void randomClassGeneratorFactoryMethod() {
+        RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator(MyClass.class);
+
+        for (int i = 0; i < 1_000; ++i) {
+            MyClass myClass = (MyClass) randomObjectGenerator.nextObject("create");
+            Assertions.assertTrue(myClass.getValue() >= -1_000 && myClass.getValue() <= 5_000);
+            Assertions.assertNotNull(myClass.getName());
+        }
+    }
+
+    @Test
+    @DisplayName("Test random record generator constructor")
     public void randomRecordGenerator() {
         RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator(MyRecord.class);
 
@@ -30,6 +42,18 @@ public class Task1Test {
             Assertions.assertTrue(myRecord.valueFirst() >= -10_000 && myRecord.valueFirst() <= 10_000);
             Assertions.assertTrue(myRecord.valueSecond() >= -10_000 && myRecord.valueSecond() <= 10_000);
             Assertions.assertNotNull(myRecord.string());
+        }
+    }
+
+    @Test
+    @DisplayName("Test random record generator factory method")
+    public void randomRecordFactoryMethod() {
+        RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator(MyRecord.class);
+
+        for (int i = 0; i < 1_000; ++i) {
+            Assertions.assertThrows(Exception.class, () -> {
+                randomObjectGenerator.nextObject("create");
+            });
         }
     }
 }
