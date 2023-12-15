12f5b31877f297165e549436364d0e33dbc08a56
EduardRK <Eduard.ivanushkin.100@gmail.com>
2023-12-15 13:57:38 +0300

    hw10

---
+++ src/main/java/edu/hw10/Task1/RandomObjectGenerator.java
@@ -0,0 +1,92 @@
+package edu.hw10.Task1;
+
+import edu.hw10.Task1.Annotations.Max;
+import edu.hw10.Task1.Annotations.Min;
+import edu.hw10.Task1.Annotations.NotNull;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomObjectGenerator implements ObjectGenerator {
+    private final ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+    private final Class<?> currentClass;
+
+    public RandomObjectGenerator(Class<?> currentClass) {
+        this.currentClass = currentClass;
+    }
+
+    @Override
+    public Object nextObject() {
+        Constructor<?>[] constructors = currentClass.getDeclaredConstructors();
+        Constructor<?> constructor = constructors[threadLocalRandom.nextInt(constructors.length)];
+        Parameter[] parameters = constructor.getParameters();
+
+        try {
+            if (parameters.length == 0) {
+                return constructor.newInstance();
+            }
+
+            return constructor.newInstance(getArguments(parameters));
+        } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Object nextObject(String factoryMethod) {
+        try {
+            Method method = Arrays.stream(currentClass.getMethods())
+                .filter(method1 -> method1.getName().equals(factoryMethod))
+                .findFirst()
+                .orElse(null);
+            Parameter[] parameters = method.getParameters();
+
+            return method.invoke(null, getArguments(parameters));
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object[] getArguments(Parameter[] parameters) {
+        Object[] arguments = new Object[parameters.length];
+        int i = 0;
+        for (Parameter parameter : parameters) {
+            int max = Integer.MAX_VALUE;
+            int min = Integer.MIN_VALUE;
+            boolean canBeNull = true;
+
+            Annotation[] annotations = parameter.getAnnotations();
+            for (Annotation annotation : annotations) {
+                if (annotation instanceof Max) {
+                    max = ((Max) annotation).value();
+                }
+                if (annotation instanceof Min) {
+                    min = ((Min) annotation).value();
+                }
+                if (annotation instanceof NotNull) {
+                    canBeNull = false;
+                }
+            }
+
+            if (parameter.getType().isPrimitive()) {
+                arguments[i] = threadLocalRandom.nextInt(min, max);
+            } else if (canBeNull) {
+                int randomValue = threadLocalRandom.nextInt(-2, 2);
+                if (randomValue >= 1) {
+                    arguments[i] = null;
+                } else {
+                    arguments[i] = UUID.randomUUID().toString();
+                }
+            } else {
+                arguments[i] = UUID.randomUUID().toString();
+            }
+            ++i;
+        }
+        return arguments;
+    }
+}
---
+++ src/test/java/edu/hw10/Task1Test.java
@@ -0,0 +1,35 @@
+package edu.hw10;
+
+import edu.hw10.Task1.Classes.MyClass;
+import edu.hw10.Task1.Classes.MyRecord;
+import edu.hw10.Task1.RandomObjectGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Task1Test {
+    @Test
+    @DisplayName("Test random class generator")
+    public void randomClassGenerator() {
+        RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator(MyClass.class);
+
+        for (int i = 0; i < 1_000; ++i) {
+            MyClass myClass = (MyClass) randomObjectGenerator.nextObject();
+            Assertions.assertTrue(myClass.getValue() >= -1_000 && myClass.getValue() <= 5_000);
+            Assertions.assertNotNull(myClass.getName());
+        }
+    }
+
+    @Test
+    @DisplayName("Test random record generator")
+    public void randomRecordGenerator() {
+        RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator(MyRecord.class);
+
+        for (int i = 0; i < 1_000; ++i) {
+            MyRecord myRecord = (MyRecord) randomObjectGenerator.nextObject();
+            Assertions.assertTrue(myRecord.valueFirst() >= -10_000 && myRecord.valueFirst() <= 10_000);
+            Assertions.assertTrue(myRecord.valueSecond() >= -10_000 && myRecord.valueSecond() <= 10_000);
+            Assertions.assertNotNull(myRecord.string());
+        }
+    }
+}
