6b9f767a2837bce69a49061ab74de2f7bc7b5c67
EduardRK <Eduard.ivanushkin.100@gmail.com>
2023-12-16 09:47:38 +0300

    Fix checkstyle

--- src/main/java/edu/hw11/Task2/ArithmaticUtils/MultiArithmeticUtils.java
+++ src/main/java/edu/hw11/Task2/ArithmaticUtils/MultiArithmeticUtils.java
@@ -1,6 +1,10 @@
 package edu.hw11.Task2.ArithmaticUtils;
 
-public class MultiArithmeticUtils {
+public final class MultiArithmeticUtils {
+    private MultiArithmeticUtils() {
+
+    }
+
     public static int operation(int a, int b) {
         return a * b;
     }
--- src/main/java/edu/hw11/Task2/ArithmaticUtils/SumArithmeticUtils.java
+++ src/main/java/edu/hw11/Task2/ArithmaticUtils/SumArithmeticUtils.java
@@ -1,6 +1,9 @@
 package edu.hw11.Task2.ArithmaticUtils;
 
-public class SumArithmeticUtils {
+public final class SumArithmeticUtils {
+    private SumArithmeticUtils() {
+    }
+
     public static int operation(int a, int b) {
         return a + b;
     }
